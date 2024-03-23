957380358c82a88b6f69119c2c35fb08c40a35ba
Maryna Bolotska <m.bolotska@icloud.com>
2024-03-23 08:09:57 +0100

    init

---
+++ ByteCoin/ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Maryna Bolotska on 23/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
