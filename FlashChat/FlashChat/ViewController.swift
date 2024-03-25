18f7efd18f2ca628746f9039f069ea3d40ac25f1
Maryna Bolotska <m.bolotska@icloud.com>
2024-03-25 12:05:02 +0100

    init

---
+++ FlashChat/FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Maryna Bolotska on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
