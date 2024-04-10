ed6b3a780074298a83214967470482af6cf69207
Maryna Bolotska <m.bolotska@icloud.com>
2024-04-10 23:09:23 +0200

    init

---
+++ Todoey/Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Maryna Bolotska on 10/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
